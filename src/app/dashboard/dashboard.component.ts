d8cce36ed22a9bc09bc2d994ccd2ce1232aa6abf
gurutvm <gurumurthi005@gmail.com>
2019-11-29 09:01:30 +0530

    dashboard tab wrapper design

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -27,7 +27,8 @@ export class DashboardComponent implements OnInit {
   yAxisLabel = 'Population';
 
   colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA', '#0099cc','#093021']
+    domain: ['#003882', '#0056a6', '#0a94d5', '#0047b8', '#00a2e0','#006ec7',
+  '#00b8e0','#008bcc', '#02abc9','#0199b9','#0099ab','#00b0c7','#2cc7d5','#7ed4de']
   };
 
   constructor() {

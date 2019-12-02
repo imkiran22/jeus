7b351af3240719d5f42f0871f5e394fcbcdf5b1d
gurutvm <gurumurthi005@gmail.com>
2019-12-02 10:31:06 +0530

    primeng and dashboard header

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { Ng5SliderModule } from 'ng5-slider';
 import { NgxChartsModule } from "@swimlane/ngx-charts";
+import { CalendarModule } from 'primeng/calendar';
 
 @NgModule({
- imports:      [ NgSelectModule, Ng5SliderModule, NgxChartsModule ],
+ imports:      [ NgSelectModule, Ng5SliderModule, NgxChartsModule,CalendarModule ],
  declarations: [  ],
- exports:      [ NgSelectModule, Ng5SliderModule, NgxChartsModule ]
+ exports:      [ NgSelectModule, Ng5SliderModule, NgxChartsModule,CalendarModule ]
 })
 export class SharedModule { }
